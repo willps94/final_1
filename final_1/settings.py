1db406123daacb641f0f58b6bc139357f3810191
Will <wtprattstephen@quinnipiac.edu>
2015-11-03 19:16:57 +0000

    Add user authentication

--- final_1/settings.py
+++ final_1/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'registration',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,3 +118,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(MAIN_DIR, 'static'),)
 STATIC_ROOT = 'staticfiles'
+LOGIN_URL = '/user/login'
+LOGIN_REDIRECT ='/'
+SITE_ID = 1
