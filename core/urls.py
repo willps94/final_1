1fdf2440c87ac3385b1682db3357feb719162ce4
Will <wtprattstephen@quinnipiac.edu>
2015-11-24 22:33:47 +0000

    Add required user authentication

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import patterns, include, url
 from .views import *
-
+from django.contrib.auth.decorators import login_required
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^user/', include('registration.backends.simple.urls')),
     url(r'^user/', include('django.contrib.auth.urls')),
-    url(r'^post_create/$', PostCreateView.as_view(), name='post_create'),
-    url(r'^post/$', PostListView.as_view(), name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post_detail'),
-    url(r'post/update/(?P<pk>\d+)/$', PostUpdateView.as_view(), name='post_update'),
-    url(r'^post/delete/(?P<pk>\d+)/$', PostDeleteView.as_view(), name='post_delete'),
-    url(r'^post/(?P<pk>\d+)/comment/create/$', CommentCreateView.as_view(), name='comment_create'),
-    url(r'^post/(?P<post_pk>\d+)/comment/update/(?P<comment_pk>\d+)/$', CommentUpdateView.as_view(), name='comment_update'),
-    url(r'^post/(?P<post_pk>\d+)/comment/delete/(?P<comment_pk>\d+)/$', CommentDeleteView.as_view(), name='comment_delete'),
+    url(r'^post_create/$', login_required(PostCreateView.as_view()), name='post_create'),
+    url(r'^post/$', login_required(PostListView.as_view()), name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', login_required(PostDetailView.as_view()), name='post_detail'),
+    url(r'post/update/(?P<pk>\d+)/$', login_required(PostUpdateView.as_view()), name='post_update'),
+    url(r'^post/delete/(?P<pk>\d+)/$', login_required(PostDeleteView.as_view()), name='post_delete'),
+    url(r'^post/(?P<pk>\d+)/comment/create/$', login_required(CommentCreateView.as_view()), name='comment_create'),
+    url(r'^post/(?P<post_pk>\d+)/comment/update/(?P<comment_pk>\d+)/$', login_required(CommentUpdateView.as_view()), name='comment_update'),
+    url(r'^post/(?P<post_pk>\d+)/comment/delete/(?P<comment_pk>\d+)/$', login_required(CommentDeleteView.as_view()), name='comment_delete'),
 )
