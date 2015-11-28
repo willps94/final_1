8700c409646294d1ffd983bce8c90c1e8474ce13
Will <wtprattstephen@quinnipiac.edu>
2015-11-28 21:49:29 +0000

    Add search

--- core/views.py
+++ core/views.py
@@ -154,24 +154,29 @@ class UserUpdateView(UpdateView):
           object = super(UserUpdateView, self).get_object(*args, **kwargs)
           if object != self.request.user:
               raise PermissionDenied()
-          return object 
-        
+          return object
+
 class UserDeleteView(DeleteView):
     model = User
     slug_field = "username"
     template_name = "user/user_confirm_delete.html"
-    
+
     def get_success_url(self):
         return reverse_lazy('logout')
-      
+
     def get_object(self, *args, **kwargs):
         object = super(UserDeleteView, self).get_object(*args, **kwargs)
         if object != self.request.user:
             raise PermissionDenied()
         return object
-      
+
     def delete(self, request, *args, **kwargs):
         user = super(UserDeleteView, self).get_object(*args)
         user.is_active = False
         user.save()
         return redirect(self.get_success_url())
+
+class SearchPostListView(PostListView):
+    def get_queryset(self):
+        incoming_query_string = self.request.GET.get('query','')
+        return Post.objects.filter(title__icontains=incoming_query_string)
